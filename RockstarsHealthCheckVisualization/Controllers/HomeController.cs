649b06b2600a7958790971005a3b4bd85f96e4a4
LaurenzPoll <laurenzvandepoll@icloud.com>
2022-11-04 13:51:23 +0100

    Added controllers

--- RockstarsHealthCheckVisualization/Controllers/HomeController.cs
+++ RockstarsHealthCheckVisualization/Controllers/HomeController.cs
@@ -37,28 +37,29 @@ namespace RockstarsHealthCheckVisualization.Controllers
             return View("Index", date);
         }
 
-        public IActionResult Privacy()
+        public IActionResult Help()
         {
             return View();
         }
-        public IActionResult MailUrl()
-        {
-            MailingViewModel mail = new MailingViewModel();
-            mail.FillQuestionnaireList();
 
-            return View(mail);
-        }
+        //public IActionResult MailUrl()
+        //{
+        //    MailingViewModel mail = new MailingViewModel();
+        //    mail.FillQuestionnaireList();
 
-        [HttpPost]
-        public IActionResult MailUrl(MailingViewModel mail)
-        {
+        //    return View(mail);
+        //}
 
-            mail.link = URL.GenerateQuestionnaireURL(mail.linkID);
-            mail.SendMail();
+        //[HttpPost]
+        //public IActionResult MailUrl(MailingViewModel mail)
+        //{
+
+        //    mail.link = URL.GenerateQuestionnaireURL(mail.linkID);
+        //    mail.SendMail();
 
 
-            return Ok("De ingetypte mail: " + mail.toEmail + "\nDe gekozen questionnaire: " + mail.linkID);
-        }
+        //    return Ok("De ingetypte mail: " + mail.toEmail + "\nDe gekozen questionnaire: " + mail.linkID);
+        //}
 
 
         [AllowAnonymous]
---
+++ RockstarsHealthCheckVisualization/Controllers/QuestionairesController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RockstarsHealthCheckVisualization.Models;
+
+namespace RockstarsHealthCheckVisualization.Controllers
+{
+    public class QuestionairesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult MailUrl()
+        {
+            MailingViewModel mail = new MailingViewModel();
+            mail.FillQuestionnaireList();
+
+            return View(mail);
+        }
+
+        [HttpPost]
+        public IActionResult MailUrl(MailingViewModel mail)
+        {
+
+            mail.link = URL.GenerateQuestionnaireURL(mail.linkID);
+            mail.SendMail();
+
+
+            return Ok("De ingetypte mail: " + mail.toEmail + "\nDe gekozen questionnaire: " + mail.linkID);
+        }
+    }
+}
