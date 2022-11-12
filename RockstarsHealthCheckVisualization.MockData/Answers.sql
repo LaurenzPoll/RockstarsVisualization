ebe04ce7e18ab0765b9b03da7af95b697daf81c1
Milena van der Molen <90328244+Milena-van-der-Molen@users.noreply.github.com>
2022-11-12 07:23:10 +0100

    add local db with table answers

---
+++ RockstarsHealthCheckVisualization.MockData/Answers.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Answers]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [FilledOutQuestionnaireId] INT NOT NULL, 
+    [QuestionId] INT NOT NULL, 
+    [UserId] INT NOT NULL, 
+    [AnswerRange] TINYINT NOT NULL, 
+    [AnswerComment] NVARCHAR(MAX) NULL, 
+    [Date] DATETIME NOT NULL
+)
