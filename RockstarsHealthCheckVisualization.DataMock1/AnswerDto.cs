7a0f0f37b8cc088a4040921bf13528fb4215b835
Milena van der Molen <90328244+Milena-van-der-Molen@users.noreply.github.com>
2022-11-13 18:35:06 +0100

    added dtatabase connection

---
+++ RockstarsHealthCheckVisualization.DataMock1/AnswerDto.cs
@@ -0,0 +1,21 @@
+﻿namespace RockstarsHealthCheckVisualization.DataMock1
+{
+    public class AnswerDto
+    {
+        public int ID { get; set; }
+        public int FilledOutQuestionnaireID { get; set; }
+        public int QuestionID { get; set; }
+        public string Question { get; set; }
+        public string QuestionCategory { get; set; }
+        public int UserID { get; set; }
+        public string UserIDstring { get; set; }    
+        public string UserName { get; set; }
+        public int AnswerRange { get; set; }
+        public string? AnswerComment { get; set; }
+        public DateTime Date { get; set; }
+
+
+
+
+    }
+}
---
+++ RockstarsHealthCheckVisualization.DataMock1/DatabaseManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RockstarsHealthCheckVisualization.DataMock1
+{
+    internal class DatabaseManager
+    {
+        public List<AnswerDto> Answers { get; set; }
+
+        public List<AnswerDto> GetAllAnswers()
+        {
+            string connectionString = "";
+        }
+
+    }
+}
