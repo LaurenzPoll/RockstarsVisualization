c644c007e6fc1928efb1655afd2b76fc05ad60ab
Milena van der Molen <90328244+Milena-van-der-Molen@users.noreply.github.com>
2022-11-13 18:38:42 +0100

    added classes to DataMock1 project: AnswerDto and DatabaseManager

--- RockstarsHealthCheckVisualization.DataMock1/DatabaseManager.cs
+++ RockstarsHealthCheckVisualization.DataMock1/DatabaseManager.cs
@@ -9,10 +9,11 @@ namespace RockstarsHealthCheckVisualization.DataMock1
     internal class DatabaseManager
     {
         public List<AnswerDto> Answers { get; set; }
+        public string connectionString = "Data Source = rockstars.database.windows.net; Initial Catalog = RockstarsDataBase; Persist Security Info = True; User ID = RockstarAdmin; Password = Rockstars!";
 
         public List<AnswerDto> GetAllAnswers()
         {
-            string connectionString = "";
+            
         }
 
     }
