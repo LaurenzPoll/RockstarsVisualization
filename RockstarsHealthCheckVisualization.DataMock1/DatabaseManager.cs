dd517126832bf5852c2b31113f452dc73e6807ce
Milena van der Molen <90328244+Milena-van-der-Molen@users.noreply.github.com>
2022-11-13 18:57:18 +0100

    added method GetAll in class DatabaseManager with try out from test table and test object

--- RockstarsHealthCheckVisualization.DataMock1/DatabaseManager.cs
+++ RockstarsHealthCheckVisualization.DataMock1/DatabaseManager.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,11 +10,23 @@ namespace RockstarsHealthCheckVisualization.DataMock1
     internal class DatabaseManager
     {
         public List<AnswerDto> Answers { get; set; }
+        public List<
         public string connectionString = "Data Source = rockstars.database.windows.net; Initial Catalog = RockstarsDataBase; Persist Security Info = True; User ID = RockstarAdmin; Password = Rockstars!";
 
         public List<AnswerDto> GetAllAnswers()
         {
-            
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                using (SqlCommand query = new SqlCommand("select * from CSVtoTableTestData"))
+                {
+                    conn.Open();
+                    var reader = query.ExecuteReader();
+                    while (reader.Read()) // stays true while reader gives back rows (until end table)
+                    {
+                        int firstColumn = reader.GetString(0);
+                    }
+                }
+            }
         }
 
     }
---
+++ RockstarsHealthCheckVisualization.DataMock1/TestObject.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RockstarsHealthCheckVisualization.DataMock1
+{
+    public class TestObject
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
