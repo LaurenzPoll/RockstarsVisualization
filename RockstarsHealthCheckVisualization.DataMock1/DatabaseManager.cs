3aa76f2bfe5e00ca8feb54961314e0ba635892f9
Milena van der Molen <90328244+Milena-van-der-Molen@users.noreply.github.com>
2022-11-13 19:02:51 +0100

    finished AddAll method with test object

--- RockstarsHealthCheckVisualization.DataMock1/DatabaseManager.cs
+++ RockstarsHealthCheckVisualization.DataMock1/DatabaseManager.cs
@@ -10,11 +10,14 @@ namespace RockstarsHealthCheckVisualization.DataMock1
     internal class DatabaseManager
     {
         public List<AnswerDto> Answers { get; set; }
-        public List<
+        public List<TestObject> TestObjects { get; set; }   
         public string connectionString = "Data Source = rockstars.database.windows.net; Initial Catalog = RockstarsDataBase; Persist Security Info = True; User ID = RockstarAdmin; Password = Rockstars!";
 
         public List<AnswerDto> GetAllAnswers()
         {
+            List<AnswerDto> Answers = new List<AnswerDto>();
+            List<TestObject> TestObjects = new List<TestObject>();
+
             using (SqlConnection conn = new SqlConnection(connectionString))
             {
                 using (SqlCommand query = new SqlCommand("select * from CSVtoTableTestData"))
@@ -23,7 +26,11 @@ namespace RockstarsHealthCheckVisualization.DataMock1
                     var reader = query.ExecuteReader();
                     while (reader.Read()) // stays true while reader gives back rows (until end table)
                     {
-                        int firstColumn = reader.GetString(0);
+                        TestObject testObject = new TestObject();
+                        testObject.Id = reader.GetInt32(0);
+                        testObject.Name = reader.GetString(1);
+
+                        TestObjects.Add(testObject);
                     }
                 }
             }
