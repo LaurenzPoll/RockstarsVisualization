1576e2c731d36ed7dcc19023f3440dc1c9176b23
Milena van der Molen <90328244+Milena-van-der-Molen@users.noreply.github.com>
2022-11-15 07:04:41 +0100

    added console app project for writing initial data to database

--- RockstarsHealthCheckVisualization.MockDataFiller/Program.cs
+++ RockstarsHealthCheckVisualization.MockDataFiller/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+Console.WriteLine("Writing data to database...");
+
+
+
+
+
+
+
+
+
+
+
+
+
+Console.WriteLine("Finished writing!")
+
+
