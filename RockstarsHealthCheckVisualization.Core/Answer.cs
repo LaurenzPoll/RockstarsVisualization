86b5586d744a2ee1cd7f62bff560335329c6d935
Milena van der Molen <90328244+Milena-van-der-Molen@users.noreply.github.com>
2022-11-25 20:56:01 +0100

    push

--- RockstarsHealthCheckVisualization.Core/Answer.cs
+++ RockstarsHealthCheckVisualization.Core/Answer.cs
@@ -2,11 +2,11 @@
 {
     internal class Answer
     {
-        private int answerID;   // key
-        private int questionID;
-        private int filledOutQuestionnaireID;
-        private int answerRange; // 1-5 for stars and maybe in future 0 for 'non applicable' (for the case of team work question when no team there)
-        private string? answerComment;    // nullable for if answerRating 2 - 4, in which case no answerComment should be added
+        public int answerID { get; private set; }   // key
+        public int questionID { get; private set; }
+        public int filledOutQuestionnaireID { get; private set; }
+        public int answerRange { get; private set; } // 1-5 for stars and maybe in future 0 for 'non applicable' (for the case of team work question when no team there)
+        public string? answerComment { get; private set; }    // nullable for if answerRating 2 - 4, in which case no answerComment should be added
 
 
         public Answer(int answerId, int questionId, int filledOutQuestionnaireId, int answerRange, int rating, string? comment)
--- RockstarsHealthCheckVisualization.Core/DtoAnswers.cs
+++ RockstarsHealthCheckVisualization.Core/DtoAnswers.cs
@@ -1,14 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace RockstarsHealthCheckVisualization.Core
 {
-    public class DtoAnswers
+    public class DTOAnswers
     {
+        private string connectionString = "Data Source=rockstars.database.windows.net;Initial Catalog=RockstarsDataBase;Persist Security Info=True;User ID=RockstarAdmin;Password=Rockstars!";
 
+        public List<Answer> GetAllAnswers()
+        {
+            List<Answer> answers = new List<Answer>();
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                using (SqlCommand query = new SqlCommand("select * from MockPlantEdible", conn))
+                {
+                    conn.Open();
+                    var reader = query.ExecuteReader();
+                    while (reader.Read()) // stays true while reader gives back rows (until end table)
+                    {
+                        int answerId = reader.GetInt32(0);
+                        int filledOutQuestionnaireId = reader.GetString(1);
+                        int questionId = reader.GetInt32(2);
+                        Answer answer = new Answer();
+                        
+                        plantEdibleCollection.Add(plantEdible);
+                    }
+                }
+            }
+
+            return plantEdibleCollection;
+        }
 
 
 
