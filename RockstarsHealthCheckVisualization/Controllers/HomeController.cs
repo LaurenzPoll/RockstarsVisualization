db69f453418fcdafb0da3ca3b4bf7b4ad7e099e5
LaurenzPoll <laurenzvandepoll@icloud.com>
2022-12-06 10:21:05 +0100

    Azure uncommented

--- RockstarsHealthCheckVisualization/Controllers/HomeController.cs
+++ RockstarsHealthCheckVisualization/Controllers/HomeController.cs
@@ -8,7 +8,7 @@ using System.Diagnostics;
 
 namespace RockstarsHealthCheckVisualization.Controllers
 {
-    //[Authorize]
+    [Authorize]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
@@ -64,31 +64,10 @@ namespace RockstarsHealthCheckVisualization.Controllers
                 }
 
                 dataPoints.Add(new DataPoint(answers.Find(x => x.questionID == avg.Key).question, Math.Round(avg.Value)));
-                /*for (int i = 0; i < answers.Count; i++)
-                {
-                    bool check = answers[i].question.Contains("[Trend]");
-                    if (!check)
-                    {
-                        dataPoints.Add(new DataPoint(answers[i].question, avg));
-                    }
-                }*/
             }
-            //😀😀😀
+
             ViewBag.DataPoints = JsonConvert.SerializeObject(dataPoints);
 
-            /*foreach (var ans in answers)
-            {
-                if (ans.answerRange > 0)
-                {
-                    range.Add(ans.answerRange);
-                    ViewBag.rangeList = range;
-                }
-                else
-                {
-                    trend.Add(ans.answerRange);
-                    ViewBag.trendList = trend;
-                }
-            }*/
 
             return View();
         }
@@ -97,26 +76,6 @@ namespace RockstarsHealthCheckVisualization.Controllers
         {
             return View();
         }
-
-        //public IActionResult MailUrl()
-        //{
-        //    MailingViewModel mail = new MailingViewModel();
-        //    mail.FillQuestionnaireList();
-
-        //    return View(mail);
-        //}
-
-        //[HttpPost]
-        //public IActionResult MailUrl(MailingViewModel mail)
-        //{
-
-        //    mail.link = URL.GenerateQuestionnaireURL(mail.linkID);
-        //    mail.SendMail();
-
-
-        //    return Ok("De ingetypte mail: " + mail.toEmail + "\nDe gekozen questionnaire: " + mail.linkID);
-        //}
-
 
         [AllowAnonymous]
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
