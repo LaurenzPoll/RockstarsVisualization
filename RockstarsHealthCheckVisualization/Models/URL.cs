e2ae4d97527a2c46f0077fc21dae7164b1667eb6
LaurenzPoll <laurenzvandepoll@icloud.com>
2022-12-20 09:59:05 +0100

    In URL changed index to Index inside the return

--- RockstarsHealthCheckVisualization/Models/URL.cs
+++ RockstarsHealthCheckVisualization/Models/URL.cs
@@ -4,7 +4,7 @@ public class URL
 {
     public static string GenerateQuestionnaireURL(int questionnaireID)
     {
-        return "https://rockstarshealthcheck.azurewebsites.net/Questionaire/index/" + questionnaireID.ToString();
+        return "https://rockstarshealthcheck.azurewebsites.net/Questionaire/Index/" + questionnaireID.ToString();
         //return "https://localhost:7080/Questionaire/index/" + questionnaireID.ToString();
     }
 }
